ddc2b69c5f575083806c2de3b7465850df270d3b
Pavitra Agarwal <pavitra@Pavitras-MacBook-Air.local>
2024-05-08 03:25:40 +0530

    deploying the entrypoint and accountfactory

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -1,3 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 import "@account-abstraction/contracts/core/EntryPoint.sol";
+import "@account-abstraction/contracts/interfaces/IAccount.sol";
+
+
+contract Account is IAccount {
+    
+    uint count = 0 ;
+    address owner ;
+    constructor (address _owner) {
+        owner = _owner ;
+    }
+    function validateUserOp(UserOperation calldata , bytes32 , uint256 )
+    external pure  returns (uint256 validationData) {
+        return  0; //initialiy we only set that all the one userops is valid 
+    }
+    function execute() external  {
+        count += 1 ;
+    }
+
+}
+
+contract AccountFactory {
+
+    function createAccount(address owner ) external returns (address) {
+        Account acc = new Account(owner) ;
+        return address(acc) ;
+    }
+}
